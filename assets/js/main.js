5b3e9c3e1db5562ce955d17fa557f6dbf37d26f0
Andre Wolf <Andre77242@hotmail.de>
2023-09-20 14:13:11 +0200

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,3 @@
-// ! ========  ERSTER VERSUCH GESCHEITERT :D ====== //
-// * Ergebnis wird nur entweder das davor oder das danach angezeigt :( //
-
-// document.getElementById("form").addEventListener("submit", (event) => {
-//   event.preventDefault();
-
-//   // Benutzereingaben abrufen
-//   const zeichenkette = document.getElementById("zeichenkette").value;
-//   const woranTrennen = document.getElementById("woranTrennen").value;
-//   const trennOption = document.querySelector(
-//     `input[name="trennOption"]:checked`
-//   ).value;
-
-//   // Zeichenkette trennen
-//   let ergebnis;
-//   if (trennOption === "davor") {
-//     ergebnis = zeichenkette.split(woranTrennen)[0];
-//   } else {
-//     ergebnis = zeichenkette.split(woranTrennen)[1];
-//   }
-
-//   // Ergebnis anzeigen
-//   const outputElement1 = document.querySelector(".output");
-//   outputElement1.textContent = ergebnis;
-// });
-
-// ! ========== ZWEITER VERSUCH ========== //
-
 // * Sumbit auf das form Element um dieses auszuführen bei Submit
 document.getElementById("form").addEventListener("submit", (event) => {
   event.preventDefault();
@@ -36,33 +8,31 @@ document.getElementById("form").addEventListener("submit", (event) => {
   const trennOption = document.querySelector(
     `input[name="trennOption"]:checked`
   ).value;
-
-  // * Zeichenkette trennen
-  let ergebnis1, ergebnis2;
-  if (trennOption === "davor") {
-    // - "davor"
-    {
-      const splitResult = zeichenkette.split(woranTrennen);
-      ergebnis1 = splitResult[0];
-      ergebnis2 = splitResult.slice(1).join(woranTrennen);
+  const davor = document.querySelector(".output");
+  const danach = document.querySelector(".output2");
+  console.log(davor);
+  console.log(danach);
+  console.log({ zeichenkette }, { woranTrennen }, { trennOption });
+  if (zeichenkette.includes(woranTrennen)) {
+    const indexTrennung = zeichenkette.indexOf(woranTrennen);
+    console.log({ indexTrennung });
+    const trennLenght = woranTrennen.length;
+    console.log(trennLenght);
+    // gib mir Index nach suchwort aus
+    const afterSearchName = indexTrennung + trennLenght;
+    console.log({ afterSearchName });
+    // hier kommen wir rein wenn er das Trennwort findet
+    // davor.innerHTML = "wort gefunden";
+    if (trennOption == "davor") {
+      davor.innerHTML = zeichenkette.slice(0, indexTrennung);
+      danach.innerHTML = zeichenkette.slice(indexTrennung);
+    } else {
+      davor.innerHTML = zeichenkette.slice(0, afterSearchName);
+      danach.innerHTML = zeichenkette.slice(afterSearchName);
     }
   } else {
-    // - "danach"
-    {
-      const index = zeichenkette.indexOf(woranTrennen);
-      if (index !== -1) {
-        ergebnis1 = zeichenkette.substring(0, index);
-        ergebnis2 = zeichenkette.substring(index + woranTrennen.length);
-      } else {
-        ergebnis1 = zeichenkette;
-        ergebnis2 = "";
-      }
-    }
+    // hier kommen wir rein wenn er das Trennwort nicht findet
+    davor.innerHTML = "fehler wort nicht gefunden";
+    danach.innerHTML = "";
   }
-
-  // * Ergebnisse anzeigen
-  const outputElement = document.querySelector(".output");
-  const output2Element = document.querySelector(".output2");
-  outputElement.textContent = ergebnis1;
-  output2Element.textContent = ergebnis2;
 });
